bf4f7f532a953f3746be9d12d724f060e0372e65
crlord <crlord@crlord.crlord>
2022-09-12 08:33:16 -0400

    Adding the router for homepage with user logged

--- app/src/router.tsx
+++ app/src/router.tsx
@@ -3,6 +3,7 @@ import { RoutePath } from 'types/routes';
 import Home from "pages/Home/index";
 import Login from "pages/Login/index"
 import Register from 'pages/Register';
+import HomeUser from 'pages/Home_User';
 
 const Router = () => {
     return (
@@ -10,6 +11,7 @@ const Router = () => {
             <Route path={RoutePath.HOME} element={<Home />} />
             <Route path={RoutePath.LOGIN} element={<Login />} />
             <Route path={RoutePath.REGISTER} element={<Register />} />
+            <Route path={RoutePath.HOME_USER} element={<HomeUser/>}/>
         </Routes>
     );
 }
