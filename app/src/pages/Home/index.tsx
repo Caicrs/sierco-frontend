cb5ea352107ee8ae2dc0a072e6e71d73ee0e6d02
crlord <crlord@crlord.crlord>
2022-09-20 19:10:04 -0400

    Insert animation on homepage

--- app/src/pages/Home/index.tsx
+++ app/src/pages/Home/index.tsx
@@ -2,22 +2,28 @@ import * as S from "./style";
 import Navbar from "components/navbar/Navbar";
 import Session1 from "./components/session_1";
 import Footer from "../../components/footer/footer";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
     <S.Home>
-      <Navbar></Navbar>
-      <Session1></Session1>
-      <S.MobileButtons>
-        <S.BtnMobile>Criar Conta</S.BtnMobile>
-        <S.H3>Ou</S.H3>
-        <S.TextContainer>
-          <S.Text to={"/login"}>
-            Já possui uma conta ? <strong>Entrar</strong>
-          </S.Text>
-        </S.TextContainer>
-      </S.MobileButtons>
-      <Footer></Footer>
+      <motion.div 
+      animate={{ opacity: [0,1],y:[-100,0]}}
+      transition={{delay:1.5}}
+      >
+        <Navbar></Navbar>
+        <Session1></Session1>
+        <S.MobileButtons>
+          <S.BtnMobile>Criar Conta</S.BtnMobile>
+          <S.H3>Ou</S.H3>
+          <S.TextContainer>
+            <S.Text to={"/login"}>
+              Já possui uma conta ? <strong>Entrar</strong>
+            </S.Text>
+          </S.TextContainer>
+        </S.MobileButtons>
+        <Footer></Footer>
+      </motion.div>
     </S.Home>
   );
 };
