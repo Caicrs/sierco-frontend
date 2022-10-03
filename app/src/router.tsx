97b8efa7622d656571be4168c20f236f61a92b6e
Caic Rocha <crlord@crlord.crlord>
2022-10-03 08:24:27 -0400

    Adding homepage route

--- app/src/router.tsx
+++ app/src/router.tsx
@@ -5,7 +5,7 @@ import Login from "pages/Login/index"
 import Register from 'pages/Register';
 import HomeUser from 'pages/Home_User';
 import Dashboard from 'pages/Dashboard';
-import GamesAdmin from 'pages/Games_Admin';
+
 
 const Router = () => {
     return (
@@ -14,8 +14,6 @@ const Router = () => {
             <Route path={RoutePath.LOGIN} element={<Login />} />
             <Route path={RoutePath.REGISTER} element={<Register />} />
             <Route path={RoutePath.HOME_USER} element={<HomeUser/>}/>
-            <Route path={RoutePath.DASHBOARD} element={<Dashboard/>}/>
-            <Route path={RoutePath.GAMESADMIN} element={<GamesAdmin/>}/>
         </Routes>
     );
 }
